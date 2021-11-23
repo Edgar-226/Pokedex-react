0a21c61579ed196f7160e67065fcef7b56966195
Edgar Bastida <edgar.bast.95@gmail.com>
2021-11-22 22:37:51 -0600

    primer commit

---
+++ src/context/pokemons/Provider.js
@@ -0,0 +1,9 @@
+import PokemonContext from "./index";
+
+export default function PokemonProvider({ children }) {
+    return (
+        <PokemonContext.Provider value={{ showAlert: () => alert(10) }}>
+            {children}
+        </PokemonContext.Provider>
+    );
+};
---
+++ src/views/404/index.js
@@ -0,0 +1,5 @@
+export default function FourOFour() {
+    return(
+        <div>404: Ruta no encontrada</div>
+    )
+}
